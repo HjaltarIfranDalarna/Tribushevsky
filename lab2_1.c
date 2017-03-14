6211f33c4e806b0646a4adeb4b7b31d31b7daeec
Vladimir <tribushevsky97@gmail.com>
2017-03-14 22:38:33 +0300

    Many comments

--- lab2_1.c
+++ lab2_1.c
@@ -5,7 +5,7 @@ int main(){
     int leap_year = 0; // Комментарий для первого коммита
     printf("Enter your age: "); // Comment for second commit
     scanf("%i", &year);
-    leap_year = year / 4;
+    leap_year = year / 4; // Many comments
     int days = year * 365 + leap_year;
     printf("Your age in days: %i\n", days);
     return 0;
