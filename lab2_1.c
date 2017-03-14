06ea22cac17323cf9b1339a170cc5ac2397ccdcc
Vladimir <tribushevsky97@gmail.com>
2017-03-14 22:45:11 +0300

    Bla

--- lab2_1.c
+++ lab2_1.c
@@ -4,7 +4,7 @@ int main(){
     int year = 0;
     int leap_year = 0; // Комментарий для первого коммита
     printf("Enter your age: "); // Comment for second commit
-    scanf("%i", &year);
+    scanf("%i", &year); // Bla
     leap_year = year / 4; // Many comments
     int days = year * 365 + leap_year;
     printf("Your age in days: %i\n", days);
